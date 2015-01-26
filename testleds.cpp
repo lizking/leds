07e809ace269ec4dd57faee92dce4ee00710a518
lizking <lizking@users.noreply.github.com>
2015-01-26 16:31:11 +0900

    Update testleds.cpp

--- testleds.cpp
+++ testleds.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+#include <string.h>
 
 #include "LedControllerRequest.h"
 #include "GeneratedTypes.h"
@@ -12,6 +13,27 @@ int main(int argc, const char **argv)
   LedControllerRequestProxy *device = new LedControllerRequestProxy(IfcNames_LedControllerRequestPortal);
 
   printf("Starting LED test");
+  FILE *pipe = popen("ifconfig eth0", "r");
+  char buf[256];
+  // read the first line and discard it
+  fgets(buf, sizeof(buf), pipe);
+  // read the second line
+  fgets(buf, sizeof(buf), pipe);
+  printf("address line: %s", buf);
+  // done with the pipe, close it
+  fclose(pipe);
+  
+  int addr[5];
+  memset(addr, 0, sizeof(addr));
+  int status = sscanf(buf, "          inet addr:%d.%d.%d.%d", &addr[0], &addr[1], &addr[2], &addr[3]);
+  printf("eth0 addr %d.%d.%d.%d\n", addr[0], addr[1], addr[2], addr[3]);
+  
+  for (int i = 0; i < 20; i++) {
+    printf("led value %x\n", addr[i % 5]);
+    device->setLeds(addr[i % 5], blinkinterval);
+    sleep(sleepinterval);
+  }
+  
 #ifdef BSIM
   // BSIM does not run very many cycles per second
   int blinkinterval = 10;
